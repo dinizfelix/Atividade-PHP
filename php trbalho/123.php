015c5019393a3f03e658e1fde8afda56e5df0e3c
Felix <59292644+dinizfelix@users.noreply.github.com>
2023-06-07 03:50:58 +0100

    Add files via upload

---
+++ php trbalho/123.php
@@ -0,0 +1,29 @@
+class Aluno {
+    private $portas;
+    public $cor;
+    private $motorista;
+}
+
+class Carro {
+    public $titulo;
+    public $autor;
+    public $numpag;
+}
+
+class Livro {
+    private $numero;
+    private $banco;
+    private $saldo;
+
+    public function depositar() {
+        // code here
+    }
+
+    public function sacar() {
+        // code here
+    }
+}
+
+class Conta {
+    // code here
+}
